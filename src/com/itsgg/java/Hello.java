bb1655504c10dbdc8f19c4e4800e04af6ea92733
yong_Qing <1633088220@qq.com>
2021-01-22 16:07:01 +0800

    Initial commit

---
+++ src/com/itsgg/java/Hello.java
@@ -0,0 +1,11 @@
+package com.itsgg.java;
+
+/**
+ * @author mxy
+ * @create 2021/1/19 - 22:15
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
---
+++ src/com/itsgg/java/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.itsgg.java;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+
+    ArrayList list = new ArrayList();
+    public void method(){
+
+    }
+}
+
+
+
+
+
